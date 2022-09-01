033aa65678b2147adc5c14c41e3679b81ea6485c
Okeibunor Favour <favourokeibunor@gmail.com>
2022-09-01 08:24:42 +0100

    add endpoint to delete user account

--- app/Controllers/Http/PetsController.ts
+++ app/Controllers/Http/PetsController.ts
@@ -1,3 +1,13 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class PetsController {}
+export default class PetsController {
+  public async fetchAllPets({}: HttpContextContract) {}
+
+  public async fetchSinglePet({}: HttpContextContract) {}
+
+  public async createPet({}: HttpContextContract) {}
+
+  public async updatePet({}: HttpContextContract) {}
+
+  public async deletePet({}: HttpContextContract) {}
+}
--- start/routes.ts
+++ start/routes.ts
@@ -21,14 +21,11 @@ Route.group(() => {
   Route.post('signup', 'UsersController.signup')
   Route.post('verify-email/:token', 'UsersController.verify')
   Route.post('resend-otp', 'UsersController.resendOtp')
-
-  Route.group(() => {
-    Route.get('details', 'UsersController.details')
-    Route.patch('/update-password', 'UsersController.updatePassword')
-    Route.patch('/', 'UsersController.update')
-  })
-    .middleware('auth')
-    .prefix('user/')
+  Route.get('verify-user/:token', 'UsersController.verify')
+  Route.post('resend-verification-link', 'UsersController.resendVerificationLink')
+  Route.post('recover-password', 'UsersController.recoverPassword')
+  Route.patch('change-password', 'UsersController.changePassword').middleware('auth')
+  Route.post('reset-password', 'UsersController.resetPassword')
 
   // social authentication
   Route.group(() => {
@@ -42,10 +39,22 @@ Route.group(() => {
     Route.get('google', 'UsersController.handleGoogleAuthCallback')
   }).prefix('callback/')
 
-  Route.get('verify-user/:token', 'UsersController.verify')
-  Route.get('details', 'UsersController.details').middleware('auth')
-  Route.post('resend-verification-link', 'UsersController.resendVerificationLink')
-  Route.post('recover-password', 'UsersController.recoverPassword')
-  Route.patch('change-password', 'UsersController.changePassword').middleware('auth')
-  Route.post('reset-password', 'UsersController.resetPassword')
+  Route.group(() => {
+    Route.get('details', 'UsersController.details')
+    Route.patch('/update-password', 'UsersController.updatePassword')
+    Route.patch('/', 'UsersController.update')
+    Route.delete('/', 'UsersController.delete')
+  })
+    .middleware('auth')
+    .prefix('user/')
+
+  Route.group(() => {
+    Route.get('/', 'PetsController.fetchAllPets')
+    Route.get('/:uuid', 'PetsController.fetchSinglePet')
+    Route.post('/', 'PetsController.createPet')
+    Route.patch('/', 'PetsController.updatePet')
+    Route.delete('/', 'PetsController.deletePet')
+  })
+    .middleware('auth')
+    .prefix('pets/')
 }).prefix('/api/v1/')
