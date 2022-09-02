8ae4d8797324fd71f6c635bdb3555a65ae9d73c5
Okeibunor Favour <favourokeibunor@gmail.com>
2022-09-02 17:29:40 +0100

    fix ts errors

--- app/Middleware/Auth.ts
+++ app/Middleware/Auth.ts
@@ -3,52 +3,48 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { unauthorizedResponse } from 'App/Helpers/Responses'
 
 export default class AuthMiddleware {
-  protected redirectTo = '/login'
+	protected redirectTo = '/login'
 
-  protected async authenticate(auth: HttpContextContract['auth'], guards: (keyof GuardsList)[]) {
-    let guardLastAttempted: string | undefined
+	protected async authenticate(auth: HttpContextContract['auth'], guards: (keyof GuardsList)[]) {
+		for (let guard of guards) {
+			if (await auth.use(guard).check()) {
+				auth.defaultGuard = guard
+				return true
+			}
+		}
+	}
 
-    for (let guard of guards) {
-      guardLastAttempted = guard
+	public async handle(
+		{ auth, request, response }: HttpContextContract,
+		next: () => Promise<void>,
+		customGuards: (keyof GuardsList)[],
+	) {
+		const guards = customGuards.length ? customGuards : [auth.name]
+		await this.authenticate(auth, guards)
 
-      if (await auth.use(guard).check()) {
-        auth.defaultGuard = guard
-        return true
-      }
-    }
-  }
-
-  public async handle(
-    { auth, request, response }: HttpContextContract,
-    next: () => Promise<void>,
-    customGuards: (keyof GuardsList)[],
-  ) {
-    const guards = customGuards.length ? customGuards : [auth.name]
-    await this.authenticate(auth, guards)
-
-    const user = auth.user
-    if (!user) {
-      return unauthorizedResponse({
-        response,
-        message: 'Please login to access',
-      })
-    }
-    if (user.status === 'approved') {
-      request.updateBody({
-        ...request.all(),
-        user,
-      })
-      await next()
-    } else if (user.status === 'pending') {
-      return unauthorizedResponse({
-        response,
-        message: `Kindly verify your account to proceed`,
-      })
-    } else if (user.status === 'banned') {
-      return unauthorizedResponse({
-        response,
-        message: `Your account has been banned, kindly contact support`,
-      })
-    }
-  }
+		const user = auth.user
+		if (!user) {
+			return unauthorizedResponse({
+				response,
+				message: 'Please login to access',
+			})
+		}
+		if (user.status === 'approved') {
+			request.updateBody({
+				...request.all(),
+				user,
+			})
+			await next()
+		} else if (user.status === 'pending') {
+			return unauthorizedResponse({
+				response,
+				message: `Kindly verify your account to proceed`,
+			})
+		} else if (user.status === 'banned') {
+			return unauthorizedResponse({
+				response,
+				message: `Your account has been banned, kindly contact support`,
+			})
+		}
+	}
 }
