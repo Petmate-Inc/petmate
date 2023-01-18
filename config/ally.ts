df3230c71014d5947105c2503b2a39c2a64d2456
Okeibunor Favour <favourokeibunor@gmail.com>
2023-01-18 15:25:02 +0100

    correct callback url

--- config/ally.ts
+++ config/ally.ts
@@ -27,7 +27,7 @@ const allyConfig: AllyConfig = {
 		driver: 'google',
 		clientId: Env.get('GOOGLE_CLIENT_ID'),
 		clientSecret: Env.get('GOOGLE_CLIENT_SECRET'),
-		callbackUrl: 'https://petmate-staging.herokuapp.com/callback/google',
+		callbackUrl: 'https://petmate-staging.herokuapp.com/api/v1/callback/google',
 	},
 	/*
   |--------------------------------------------------------------------------
@@ -38,7 +38,7 @@ const allyConfig: AllyConfig = {
 		driver: 'facebook',
 		clientId: Env.get('FACEBOOK_CLIENT_ID'),
 		clientSecret: Env.get('FACEBOOK_CLIENT_SECRET'),
-		callbackUrl: 'https://petmate-staging.herokuapp.com/callback/facebook',
+		callbackUrl: 'https://petmate-staging.herokuapp.com/api/v1/callback/facebook',
 	},
 }
 
