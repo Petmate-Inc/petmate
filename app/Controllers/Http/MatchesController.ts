cbf5082a4b86e818e0ff06f878462789453d9d72
O-tega <tega.myview@gmail.com>
2023-04-07 16:22:04 +0100

    included accpeted filter in the getMatch function

--- app/Controllers/Http/MatchesController.ts
+++ app/Controllers/Http/MatchesController.ts
@@ -48,9 +48,10 @@ export default class MatchesController {
 			})
 		}
 	}
-	public async getMatches({ auth, response }: HttpContextContract) {
+	public async getMatches({ auth, request, response }: HttpContextContract) {
 		try {
 			const user: User | null = auth.user ?? null
+			const {accepted} = await request.validate(UpdateMatchValidator)
 
 			if (!user) {
 				Logger.error({ err: new Error('Not found') }, 'user is not found')
@@ -59,6 +60,7 @@ export default class MatchesController {
 
 			const matches = await Match.query()
 				.where('breeder_uuid', user.uuid)
+				.where('accepted', accepted)
 				.whereNull('deleted_at')
 				.preload('breeder')
 				.preload('pet')
