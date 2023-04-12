61ee7bfed1304c9b03495dd5076db5bfeccaebef
O-tega <tega.myview@gmail.com>
2023-04-12 12:29:46 +0100

    remove the .then.catch and using await

--- app/Controllers/Http/VetsController.ts
+++ app/Controllers/Http/VetsController.ts
@@ -1,32 +1,27 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { badRequestResponse } from 'App/Helpers/Responses'
+import { badRequestResponse, successfulResponse } from 'App/Helpers/Responses'
 import Logger from '@ioc:Adonis/Core/Logger'
 import geolocation from 'geolocation'
+import nearbySearch from 'App/Services/apis/googleMapApi'
 
 export default class VetsController {
 	public async vetClinics({ response }: HttpContextContract) {
 		try {
-            const navigate = geolocation.getCurrentPosition
-            const lat = navigate.latitude
-            const lng = navigate.longitude
+			const navigate = geolocation.getCurrentPosition
+			const lat = navigate.latitude
+			const lng = navigate.longitude
 
-			const axios = require('axios')
+			const getNearbyLocation = await nearbySearch({
+				url: `location=${lat}%${lng}&radius=1500&type=veterinary_care&keyword=vet_clinics&key=YOUR_API_KEY`,
+				method: 'GET',
+			})
 
-			let config = {
-				method: 'get',
-				url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522%2C151.1957362&radius=1500&type=veterinary_care&keyword=vet_clinics&key=YOUR_API_KEY',
-				headers: {},
-                // The google url has a default longitude and latitude so I did this, I don't know if its correct though I couldn't find such in the documentation
-				url2: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%${lng}&radius=1500&type=veterinary_care&keyword=vet_clinics&key=YOUR_API_KEY`,
-			}
-
-			axios(config)
-				.then(function (response) {
-					console.log(JSON.stringify(response.data))
-				})
-				.catch(function (error) {
-					console.log(error)
-				})
+            const data = getNearbyLocation.data
+            return successfulResponse({
+                response,
+                message: "successful response",
+                data
+            })
 		} catch (error) {
 			Logger.error({ err: error }, 'could not get location')
 			return badRequestResponse({
