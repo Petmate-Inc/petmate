7832e54003654fb85cfa90b760557c0504ffe57d
O-tega <tega.myview@gmail.com>
2023-04-13 13:43:39 +0100

    added the api key to the env file

--- app/Controllers/Http/VetsController.ts
+++ app/Controllers/Http/VetsController.ts
@@ -3,16 +3,20 @@ import { badRequestResponse, successfulResponse } from 'App/Helpers/Responses'
 import Logger from '@ioc:Adonis/Core/Logger'
 import geolocation from 'geolocation'
 import nearbySearch from 'App/Services/apis/googleMapApi'
+import Env from '@ioc:Adonis/Core/Env'
 
 export default class VetsController {
 	public async vetClinics({ response }: HttpContextContract) {
+
+		const API_KEY = Env.get('GOOGOE_NEARBY_API')
+
 		try {
 			const navigate = geolocation.getCurrentPosition
 			const lat = navigate.latitude
 			const lng = navigate.longitude
 
 			const getNearbyLocation = await nearbySearch({
-				url: `location=${lat}%${lng}&radius=1500&type=veterinary_care&keyword=vet_clinics&key=YOUR_API_KEY`,
+				url: `location=${lat}%${lng}&radius=1500&type=veterinary_care&keyword=vet_clinics&key=${API_KEY}`,
 				method: 'GET',
 			})
 
